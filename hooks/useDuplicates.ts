051142005cce71c39c96bae7cd1e0f32965f66d0
Umer Qurreshi <u.qurreshi34@gmail.com>
2026-05-05 17:42:54 +0100

    Fix dupe finder to scan non standard folders too

--- hooks/useDuplicates.ts
+++ hooks/useDuplicates.ts
@@ -23,6 +23,12 @@ const SCAN_DIRS = [
   'file:///storage/emulated/0/Movies/',
   'file:///storage/emulated/0/DCIM/',
   'file:///storage/emulated/0/Music/',
+  'file:///storage/emulated/0/Recordings/',
+  'file:///storage/emulated/0/Ringtones/',
+  'file:///storage/emulated/0/Alarms/',
+  'file:///storage/emulated/0/Notifications/',
+  'file:///storage/emulated/0/Audiobooks/',
+  'file:///storage/emulated/0/Podcasts/',
   'file:///storage/emulated/0/Android/media/',
 ];
 
@@ -67,7 +73,7 @@ export function useDuplicates() {
     try {
       const allFiles: DuplicateFile[] = [];
 
-      const STANDARD_ROOT_DIRS = ['Download', 'Documents', 'Pictures', 'Movies', 'Music', 'DCIM', 'Recordings', 'Android'];
+      const STANDARD_ROOT_DIRS = ['Download', 'Documents', 'Pictures', 'Movies', 'Music', 'DCIM', 'Recordings', 'Ringtones', 'Alarms', 'Notifications', 'Audiobooks', 'Podcasts', 'Android'];
       const dynamicDirs = [...SCAN_DIRS];
       try {
         const rootItems = await RNFS.readDir('/storage/emulated/0/');
