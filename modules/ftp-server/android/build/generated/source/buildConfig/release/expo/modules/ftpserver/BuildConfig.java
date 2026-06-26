115c28f046ac11ddf1da3859b3424b6fcb5ef3a3
Umer Qurreshi <u.qurreshi34@gmail.com>
2026-06-27 00:19:07 +0100

    fix: add ProGuard dontwarn rules for PdfBox JP2 and SLF4J missing classes

---
+++ modules/ftp-server/android/build/generated/source/buildConfig/release/expo/modules/ftpserver/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package expo.modules.ftpserver;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "expo.modules.ftpserver";
+  public static final String BUILD_TYPE = "release";
+}
