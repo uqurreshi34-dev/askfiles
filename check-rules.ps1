4ddee768269a80e99b6275b82fdf24dc2fbb6f71
Umer Qurreshi <u.qurreshi34@gmail.com>
2026-07-22 03:21:15 +0100

    chore: add r8 rules check file to see if there are ever any missing r8 rules

---
+++ check-rules.ps1
@@ -0,0 +1,9 @@
+$dir = "android\app\build\outputs\mapping\release"
+if (-not (Test-Path $dir)) {
+    Write-Host "STOP: mapping\release missing - wrong folder or R8 did not run. NOT verified." -ForegroundColor Red
+} elseif (Test-Path "$dir\missing_rules.txt") {
+    Write-Host "missing_rules.txt EXISTS - add these to proguard-rules.pro before shipping:" -ForegroundColor Yellow
+    Get-Content "$dir\missing_rules.txt"
+} else {
+    Write-Host "CLEAN - mapping dir present, no missing_rules. R8 resolved everything." -ForegroundColor Green
+}
