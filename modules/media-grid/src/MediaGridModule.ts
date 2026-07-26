3bab7a8233bec07d7a2f5e8619b03f3d46caeb34
Umer Qurreshi <u.qurreshi34@gmail.com>
2026-07-26 21:56:07 +0100

    chore: remove media-grid module from public repo

--- modules/media-grid/src/MediaGridModule.ts
+++
@@ -1,5 +0,0 @@
-import { NativeModule, requireNativeModule } from 'expo';
-
-declare class MediaGridModule extends NativeModule<Record<string, never>> {}
-
-export default requireNativeModule<MediaGridModule>('MediaGrid');
